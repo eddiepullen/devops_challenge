6375d54d461611e6b2928c3721ba449e9dcc4964
Eddie <edward.pullen@deimos.co.za>
2023-07-18 17:40:17 +0200

    devops-challenge reverting changes issue persists

--- infrastructure/ec2.tf
+++ infrastructure/ec2.tf
@@ -31,7 +31,7 @@ resource "null_resource" "my_instance" {
     connection {
       type        = "ssh"
       user        = "ubuntu"
-      private_key = file("./ansible/config/keys/ansible-ssh-key.pem")
+      private_key = file("${path.module}/ansible/config/keys/ansible-ssh-key.pem")
       host        = module.ansible_instance.public_ip
     }
     
@@ -46,7 +46,7 @@ resource "null_resource" "my_instance" {
     connection {  
       type        = "ssh"
       user        = "ubuntu"
-      private_key = file("$./ansible/config/keys/ansible-ssh-key.pem")
+      private_key = file("${path.module}/ansible/config/keys/ansible-ssh-key.pem")
       host        = module.ansible_instance.public_ip
     }
     
