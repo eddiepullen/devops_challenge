6ddff47262defcb7bc5a651b4a03dae47246080d
Eddie <edward.pullen@deimos.co.za>
2023-07-19 17:33:11 +0200

    devops-challenge created part of the db anslbe conig, trying to get workflow to run

--- infrastructure/ansible_bootstrap.tf
+++ infrastructure/ansible_bootstrap.tf
@@ -7,6 +7,7 @@ resource "null_resource" "my_instance" {
   triggers = {
     # Will run each time there is a change to this instance.
     # instance_ids = module.ansible_instance.id
+    
     # Will run every time
     time = timestamp()
   }
@@ -23,7 +24,7 @@ resource "null_resource" "my_instance" {
   }
 
   provisioner "local-exec" {
-    command = "ansible-playbook --private-key=${path.module}/ansible/config/keys/ansible-ssh-key.pem --ssh-common-args='-o StrictHostKeyChecking=no' ${path.module}/ansible/config/master.yaml -u ubuntu -i '${module.ansible_instance.public_ip},' "
+    command = "ansible-playbook --private-key=./ansible/config/keys/ansible-ssh-key.pem --ssh-common-args='-o StrictHostKeyChecking=no' ./ansible/config/master.yaml -u ubuntu -i '${module.ansible_instance.public_ip},' "
   }
 
   # provisioner "remote-exec" {
@@ -48,14 +49,14 @@ resource "null_resource" "my_instance" {
   #   inline = ["ansible-playbook -i ~/ansible/hosts ~/ansible/playbooks/microservice.yaml --ssh-common-args='-o StrictHostKeyChecking=no'"]
   # }
 
-  provisioner "remote-exec" {
-    connection {  
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file("${path.module}/ansible/config/keys/db-ssh-key.pem")
-      host        = module.ansible_instance.public_ip
-    }
+  # provisioner "remote-exec" {
+  #   connection {  
+  #     type        = "ssh"
+  #     user        = "ubuntu"
+  #     private_key = file("${path.module}/ansible/config/keys/ansible-ssh-key.pem")
+  #     host        = module.ansible_instance.public_ip
+  #   }
     
-    inline = ["ansible-playbook -i ~/ansible/hosts ~/ansible/playbooks/db.yaml --ssh-common-args='-o StrictHostKeyChecking=no'"]
-  }
+  #   inline = ["ansible-playbook -i ~/ansible/hosts ~/ansible/playbooks/db.yaml --ssh-common-args='-o StrictHostKeyChecking=no'"]
+  # }
 }
