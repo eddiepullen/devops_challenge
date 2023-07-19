c22a235a01578e448c364a2875b222936eb8207b
Eddie <edward.pullen@deimos.co.za>
2023-07-19 19:14:14 +0200

    devops-challenge debugging workflow

--- infrastructure/ansible_bootstrap.tf
+++ infrastructure/ansible_bootstrap.tf
@@ -16,7 +16,7 @@ resource "null_resource" "my_instance" {
     connection {
       type        = "ssh"
       user        = "ubuntu"
-      private_key = file("${path.module}/ansible/config/keys/ansible-ssh-key.pem")
+      private_key = data.local_file.ansible_key.content
       host        = module.ansible_instance.public_ip
     }
     
