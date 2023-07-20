91c19aa1bca577f069c4778107d49e95bd500be9
Eddie <edward.pullen@deimos.co.za>
2023-07-20 19:42:56 +0200

    devops-challenge testing ssh port chagne and working on workflow issue

--- infrastructure/ansible_bootstrap.tf
+++ infrastructure/ansible_bootstrap.tf
@@ -12,17 +12,17 @@ resource "null_resource" "my_instance" {
     time = timestamp()
   }
 
-  # Connect to ansible control instance to to check if ssh connection is ready
-  provisioner "remote-exec" {
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = data.local_file.ansible_key.content
-      host        = module.ansible_instance.public_ip
-    }
+  # # Connect to ansible control instance to to check if ssh connection is ready
+  # provisioner "remote-exec" {
+  #   connection {
+  #     type        = "ssh"
+  #     user        = "ubuntu"
+  #     private_key = data.local_file.ansible_key.content
+  #     host        = module.ansible_instance.public_ip
+  #   }
     
-    inline = ["echo 'connected!'"]
-  }
+  #   inline = ["echo 'connected!'"]
+  # }
 
   # Run ansible against the ansible control node and bootstrap it with ansible and required worker config
   provisioner "local-exec" {
@@ -41,27 +41,27 @@ resource "null_resource" "my_instance" {
     inline = ["ansible-playbook -i ~/ansible/hosts ~/ansible/playbooks/microservice.yaml --ssh-common-args='-o StrictHostKeyChecking=no'"]
   }
 
-  # Tell ansible control node to  run against db and configure it
-  provisioner "remote-exec" {
-    connection {  
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file("${path.module}/ansible/config/keys/ansible-ssh-key.pem")
-      host        = module.ansible_instance.public_ip
-    }
+  # # Tell ansible control node to  run against db and configure it
+  # provisioner "remote-exec" {
+  #   connection {  
+  #     type        = "ssh"
+  #     user        = "ubuntu"
+  #     private_key = file("${path.module}/ansible/config/keys/ansible-ssh-key.pem")
+  #     host        = module.ansible_instance.public_ip
+  #   }
     
-    inline = ["ansible-playbook -i ~/ansible/hosts ~/ansible/playbooks/db.yaml --ssh-common-args='-o StrictHostKeyChecking=no'"]
-  }
+  #   inline = ["ansible-playbook -i ~/ansible/hosts ~/ansible/playbooks/db.yaml --ssh-common-args='-o StrictHostKeyChecking=no'"]
+  # }
 
-  # Tell control node to run ansible against the lb and configure it
-  provisioner "remote-exec" {
-    connection {  
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file("${path.module}/ansible/config/keys/ansible-ssh-key.pem")
-      host        = module.ansible_instance.public_ip
-    }
+  # # Tell control node to run ansible against the lb and configure it
+  # provisioner "remote-exec" {
+  #   connection {  
+  #     type        = "ssh"
+  #     user        = "ubuntu"
+  #     private_key = file("${path.module}/ansible/config/keys/ansible-ssh-key.pem")
+  #     host        = module.ansible_instance.public_ip
+  #   }
     
-    inline = ["ansible-playbook -i ~/ansible/hosts ~/ansible/playbooks/lb.yaml --ssh-common-args='-o StrictHostKeyChecking=no'"]
-  }
+  #   inline = ["ansible-playbook -i ~/ansible/hosts ~/ansible/playbooks/lb.yaml --ssh-common-args='-o StrictHostKeyChecking=no'"]
+  # }
 }
