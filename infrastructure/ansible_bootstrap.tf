17006b448a44d80a0917d13919e30e711345d2bd
Eddie <edward.pullen@deimos.co.za>
2023-07-21 09:23:16 +0200

    devops-challenge fixing workflow

--- infrastructure/ansible_bootstrap.tf
+++ infrastructure/ansible_bootstrap.tf
@@ -3,6 +3,7 @@
 # control node and then use Terraform to tell the control node to configure
 # the worker nodes and pull and run the images from ECR on the microservice
 resource "null_resource" "my_instance" {
+
   triggers = {
     # Will run each time there is a change to this instance.
     # instance_ids = module.ansible_instance.id
@@ -10,6 +11,7 @@ resource "null_resource" "my_instance" {
     # Will run every time
     time = timestamp()
   }
+
   # Connect to ansible control instance to to check if ssh connection is ready
   provisioner "remote-exec" {
     connection {
@@ -19,41 +21,45 @@ resource "null_resource" "my_instance" {
       private_key = file(var.ansible_ssh_key)
       host        = module.ansible_instance.public_ip
     }
-
+    
     inline = ["echo 'connected!'"]
   }
+
   # Run ansible against the ansible control node and bootstrap it with ansible and required worker config
   provisioner "local-exec" {
-    command = "ansible-playbook --private-key=${file(var.ansible_ssh_key)} --ssh-common-args='-o StrictHostKeyChecking=no' ./ansible/config/master.yaml -u ubuntu -i '${module.ansible_instance.public_ip},' "
+    command = "ansible-playbook --private-key=${path.module}/ansible/config/keys/ansible-ssh-key.pem --ssh-common-args='-o StrictHostKeyChecking=no' ./ansible/config/master.yaml -u ubuntu -i '${module.ansible_instance.public_ip},' "
   }
+
   # Tell ansible control node to run against microservice and configure it, pull and run the imgaes
   provisioner "remote-exec" {
     connection {  
       type        = "ssh"
       user        = "ubuntu"
-      private_key = file(var.ansible_ssh_key)
+      private_key = file("${path.module}/ansible/config/keys/ansible-ssh-key.pem")
       host        = module.ansible_instance.public_ip
     }
     
     inline = ["ansible-playbook -i ~/ansible/hosts ~/ansible/playbooks/microservice.yaml --ssh-common-args='-o StrictHostKeyChecking=no'"]
   }
+
   # Tell ansible control node to  run against db and configure it
   provisioner "remote-exec" {
     connection {  
       type        = "ssh"
       user        = "ubuntu"
-      private_key = file(var.ansible_ssh_key)
+      private_key = file("${path.module}/ansible/config/keys/ansible-ssh-key.pem")
       host        = module.ansible_instance.public_ip
     }
     
     inline = ["ansible-playbook -i ~/ansible/hosts ~/ansible/playbooks/db.yaml --ssh-common-args='-o StrictHostKeyChecking=no'"]
   }
+
   # Tell control node to run ansible against the lb and configure it
   provisioner "remote-exec" {
     connection {  
       type        = "ssh"
       user        = "ubuntu"
-      private_key = file(var.ansible_ssh_key)
+      private_key = file("${path.module}/ansible/config/keys/ansible-ssh-key.pem")
       host        = module.ansible_instance.public_ip
     }
     
